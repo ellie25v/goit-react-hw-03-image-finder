0ea6019de7951ba7522a6f9ac781b44b1808c919
ellie25v <yelizarieva.ellie@gmail.com>
2020-03-17 14:18:30 +0200

    Updates

---
+++ precache-manifest.ac307dea9c1c2f97c0eb81235d8df5ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e6e280ebcf1968fd524fc5b4379350",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0cadb6b144ba8ec851a9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6700d076.chunk.css"
+  },
+  {
+    "revision": "e42e82d5bec9b3c8a99f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b3b6c81f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b3b6c81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cadb6b144ba8ec851a9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.22ccc041.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
